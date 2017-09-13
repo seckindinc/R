f513eff42bd734f0906a1485b5061aea9696fa5c
Seckin Dinc <SDINC@gelirler.gov.tr>
2017-09-13 17:04:00 +0300

    Logistic Regression Commit

--- Logistic_Regression/Logistic_Regression.R
+++ Logistic_Regression/Logistic_Regression.R
@@ -1,14 +1,10 @@
 #Package Loading
 library(readr)
-# library(bootstrap)
-# library(MASS)
-# library(DAAG)
-# library(relaimpo)
 library(ggplot2)
 library(corrplot)
-# library(lmtest)
-# library(Hmisc)
-# library(car)
+library(caret)
+library(ROCR)
+library(Metrics)
 
 #Step 1: Data Understanding
 
@@ -27,9 +23,10 @@ train <- subset(Risk[dt,], select=-ID)
 test <- subset(Risk[-dt,], select=-ID)
 
 #Categorizing RISK column
-train$RISK_Category[train$RISK == "good risk"] <- 1
-train$RISK_Category[train$RISK != "good risk"] <- 2
-train$RISK_Category <- factor(train$RISK_Category,levels=c(1,2),labels=c("Good","Bad"))
+train$RISK_Category <- ifelse(train$RISK == "good risk",1,0)
+train$RISK_Category <- factor(train$RISK_Category,levels=c(1,0),labels=c("Good","Bad"))
+test$RISK_Category <- ifelse(test$RISK == "good risk",1,0)
+test$RISK_Category <- factor(test$RISK_Category,levels=c(1,0),labels=c("Good","Bad"))
 train <- subset(train, select=-RISK)
 
 #Selecting Integer Fields excep ID column
@@ -121,10 +118,10 @@ for(i in names(integer_fields)) {
 #Step 3: Modelling
 
 #Multiple Linear Regression
-multiple_logistic_model = glm(RISK_Category~. ,data=train, family=binomial())
+multiple_logistic_model_full = glm(RISK_Category~. ,data=train, family=binomial(link = "logit"))
 
 #Summary
-summary(multiple_logistic_model)
+summary(multiple_logistic_model_full)
  
 # Deviance Residuals: 
 #   Min       1Q   Median       3Q      Max  
@@ -155,176 +152,51 @@ summary(multiple_logistic_model)
 # 
 # Number of Fisher Scoring iterations: 6
 
-#Stepwise regression
-stepwise_model <- stepAIC(multiple_linear_model, direction = "both")
+#Removing non significant variables
+multiple_logistic_model_reduced = glm(RISK_Category~AGE+INCOME+MARITAL+NUMKIDS+NUMCARDS+HOWPAID+MORTGAGE+STORECAR+LOANS ,data=train, family=binomial())
 
-#General Model Statistics
-summary(stepwise_model)
-#At variable selection phase we need to investigate every single variable to enter the model or not.
-# H0 : β1 = 0
-# Ha : β1 <> 0
-# Coefficients:
-#   Estimate Std. Error t value Pr(>|t|)    
-# (Intercept) 19626.60     423.93  46.296  < 2e-16 ***
-#   AGE           503.41      14.09  35.718  < 2e-16 ***
-#   NUMKIDS     -1089.20     146.88  -7.416 1.46e-13 ***
-#   NUMCARDS     -151.78      97.88  -1.551    0.121    
-# STORECAR    -1547.29     118.49 -13.058  < 2e-16 ***
-#   LOANS       -3068.11     194.85 -15.746  < 2e-16 ***
-#By the t statistics we accept H0 for NUMCARDS and reject H0 for other variables.
+#Model Summary
+summary(multiple_logistic_model_reduced)
 
-#By this we come to the relation between the predictors and response variable.
-#H0 : β 1 = β 2 = · · · = β p = 0
-#Ha : at least one β j is non-zero.
-#By the f statistics we reject H0
+#compare two models
+#With p > 0.05, this ANOVA test also corroborates the fact that the second model is better than first model.
+anova(multiple_logistic_model_full,multiple_logistic_model_reduced,test = "Chisq")
 
-#1)The regression model is linear in parameters
+# Performance of Logistic Regression Model
+#1. AIC (Akaike Information Criteria) – The analogous metric of adjusted R² in logistic regression 
+#is AIC. AIC is the measure of fit which penalizes model for the number of model coefficients. 
+#Therefore, we always prefer model with minimum AIC value.
+#2. Null Deviance and Residual Deviance – Null Deviance indicates the response predicted by a 
+#model with nothing but an intercept. Lower the value, better the model. 
+#Residual deviance indicates the response predicted by a model on adding independent variables. 
+#Lower the value, better the model.
 
-#2) The mean of residuals is zero
-#If it zero (or very close), then this assumption is held true for that model. 
-mean(stepwise_model$residuals)
+#3. Confusion Matrix: It is nothing but a tabular representation of Actual vs Predicted values. 
+#This helps us to find the accuracy of the model and avoid overfitting. This is how it looks like:
 
-#3) Homoscedasticity of residuals or equal variance
-#Residuals vs Fitted and Scale-Location graphs are used for this assumption.
-#Residuals vs Fitted: As the fitted values along x increase, the residuals decrease and then increase. 
-#This pattern is indicated by the red line, which should be approximately flat if the disturbances are homoscedastic. 
-#Scale-Location: Is more convenient as the disturbance term in Y axis is standardized.
-layout(matrix(c(1,2,3,4),2,2))
-plot(stepwise_model)
+#Response probabilty
+train$predict <- multiple_logistic_model_reduced$fitted.values
+#Response category
+train$RISK_Category_predict <- ifelse(train$predict > 0.50, "Good","Bad")
+#Confusion Matrix
+confusionMatrix(train[["RISK_Category_predict"]], train[["RISK_Category"]])
 
-#4) No autocorrelation of residuals
-#This is applicable especially for time series data. Autocorrelation is the correlation of a time Series with lags of itself. 
-#When the residuals are autocorrelated, it means that the current value is dependent of the previous (historic) values 
-#and that there is a definite unexplained pattern in the Y variable that shows up in the disturbances.
+#4. ROC Curve: Receiver Operating Characteristic(ROC) summarizes the model’s performance by evaluating
+#the trade offs between true positive rate (sensitivity) and false positive rate(1- specificity). 
+# For plotting ROC, it is advisable to assume p > 0.5 since we are more concerned about success rate. 
+# ROC summarizes the predictive power for all possible values of p > 0.5.  
+# The area under curve (AUC), referred to as index of accuracy(A) or concordance index, 
+# is a perfect performance metric for ROC curve. 
+# Higher the area under curve, better the prediction power of the model. Below is a sample ROC curve. 
+# The ROC of a perfect predictive model has TP equals 1 and FP equals 0. 
+# This curve will touch the top left corner of the graph.
 
-# Durbin-Watson test
-#H0 : Autocorrelation is 0
-#Ha : Autocorrelation is greater than 0
-dwtest(stepwise_model)
+ROCRpred <- prediction(train$predict, train$RISK_Category)
+ROCRperf <- performance(ROCRpred, 'tpr','fpr')
+plot(ROCRperf) > auc(train$RISK_Category,train$predict)
 
-#5) The X variables and residuals are uncorrelated
-for(i in names(subset(train, select=-INCOME))) {
-  cat ("Correlation Between ",i," and Residuals Test")
-  print (cor.test(as.numeric(unlist(train[,i])),stepwise_model$residuals))
-}
-# Correlation Between  AGE  and Residuals Test
-# Pearson's product-moment correlation
-# 
-# data:  as.numeric(unlist(predictors[, i])) and multiple_linear_model$residuals
-# t = 2.3845e-14, df = 4115, p-value = 1
-# alternative hypothesis: true correlation is not equal to 0
-# 95 percent confidence interval:
-# -0.03054786  0.03054786
-# sample estimates:
-# cor 
-# 3.71716e-16 
+#Test Data Prediction
+test$predict <- predict(multiple_logistic_model_reduced,newdata = test,type = "response")
+test$RISK_Category_predict <- ifelse(test$predict > 0.5, "Good","Bad")
 
-#6) The number of observations must be greater than number of Xs
-
-#7) The variability in X values is positive
-for(i in names(subset(train, select=-INCOME))) {
-  cat ("Variance of ",i)
-  print (var(as.numeric(unlist(train[,i]))))
-}
-# Variance of  AGE[1] 97.54978
-# Variance of  NUMKIDS[1] 1.370254
-# Variance of  NUMCARDS[1] 3.537112
-# Variance of  STORECAR[1] 1.829982
-# Variance of  LOANS[1] 0.7025512
-
-#8) No perfect multicollinearity
-#This can be handled by Variance Inflaction Factor
-#VIF is a metric computed for every X variable that goes into a linear model. 
-#If the VIF of a variable is high, it means the information in that variable is already explained by other X variables present in the given model, 
-#which means, more redundant is that variable. So, lower the VIF (<2) the better.
-sqrt(vif(stepwise_model))
-# AGE  NUMKIDS NUMCARDS STORECAR    LOANS 
-# 1.708857 2.606943 2.988686 2.265843 2.352308
-
-#9) Normality of residuals
-print(shapiro.test(stepwise_model$residuals))
-
-#Hat Leverage Point
-train <- cbind(train,hat(model.matrix(stepwise_model)) > 0.2)
-names(train)[length(names(train))] <- paste("hat_leverage",sep="") 
-
-#Cook's Distance
-train <- cbind(train,cooks.distance(stepwise_model) > 1)
-names(train)[length(names(train))] <- paste("cook_distance",sep="") 
-
-#Variable Importance
-calc.relimp(stepwise_model,rela=TRUE)
-
-#Cross Validation
-# In cross-validation, a portion of the data is selected as the training sample and a
-# portion is selected as the hold-out sample. A regression equation is developed on
-# the training sample, and then applied to the hold-out sample. Because the hold-out
-# sample wasn’t involved in the selection of the model parameters, the performance on
-# this sample is a more accurate estimate of the operating characteristics of the model
-# with new data.
-
-#4)Model Evaluation
-
-#RMSE train
-train$prediction <- stepwise_model$fitted.values
-train$residuals <- stepwise_model$residuals
-rmse_train <- sqrt(mean(train$residuals^2))
-
-#INCOME mean
-INCOME_train_mean <- mean(train$INCOME)
-#Total Sum of Squares
-tss_train <- sum((train$INCOME - INCOME_train_mean)^2)
-#Residual Sum of Squares
-rss_train <- sum(stepwise_model$residuals^2)
-#R-squared
-rsq_train <- 1 - (rss_train/tss_train)
-
-#Correlation between actual value and prediction
-print (cor.test(train$INCOME,train$prediction))
-
-#Train vs Test
-
-#Test Prediction
-test$prediction <- predict(stepwise_model, newdata = test)
-#INCOME mean
-INCOME_test_mean <- mean(test$INCOME)
-#Total Sum of Squares
-tss_test <- sum((test$INCOME - INCOME_test_mean)^2)
-#Residuals
-test$residuals <- test$INCOME - test$prediction
-#Residual Sum of Squares
-rss_test <- sum(test$residuals^2)
-#R-squared
-rsq_test <- 1 - (rss_test/tss_test)
-#RMSE Test
-rmse_test <- sqrt(mean(test$residuals^2))
-
-# Evaluate the rmse on both training and test data and print them
-rmse_train
-rmse_test
-
-# Evaluate the r-squared on both training and test data.and print them
-rsq_train
-rsq_test
-
-# Plot the predictions (on the x-axis) against the outcome (cty) on the test data
-ggplot(train, aes(x = prediction, y = INCOME)) +   geom_point() +   geom_abline()
-ggplot(test, aes(x = prediction, y = INCOME)) +   geom_point() +   geom_abline()
-
-shrinkage <- function(fit, k=10){
-  theta.fit <- function(x,y){lsfit(x,y)}
-  theta.predict <- function(fit,x){cbind(1,x)%*%fit$coef}
-  x <- fit$model[,2:ncol(fit$model)]
-  y <- fit$model[,1]
-  results <- crossval(x, y, theta.fit, theta.predict, ngroup=k)
-  r2 <- cor(y, fit$fitted.values)^2
-  r2cv <- cor(y, results$cv.fit)^2
-  cat("Original R-square =", r2, "\n")
-  cat(k, "Fold Cross-Validated R-square =", r2cv, "\n")
-  cat("Change =", r2-r2cv, "\n")
-}
-
-shrinkage(stepwise_model)
-
-#5) Deployment
-stepwise_model$coefficients
+confusionMatrix(test[["RISK_Category_predict"]], test[["RISK_Category"]])
