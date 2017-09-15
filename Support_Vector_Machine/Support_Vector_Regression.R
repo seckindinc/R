408bd50e4395223f90418d7c5ace96580b231a5d
Seckin Dinc <SDINC@gelirler.gov.tr>
2017-09-15 09:25:03 +0300

    File Rename

---
+++ Support_Vector_Machine/Support_Vector_Regression.R
@@ -0,0 +1,69 @@
+#Package Loading
+library(readr)
+library(e1071)
+
+#Support Vector Regression
+
+#Step 1: Data Understanding
+
+#Gathering Data
+Risk <- read_delim("/home/seckindinc/Desktop/Projects/R/Data/Risk.txt", "\t", escape_double = FALSE, trim_ws = TRUE)
+
+#Structre of Data
+str(Risk)
+
+#Summary of Data
+summary(Risk)
+
+#Selecting Integer Fields excep ID column
+integer_fields = subset(Risk[,sapply(Risk, is.numeric)], select=-ID)
+
+#Train and Test Data
+dt = sort(sample(nrow(integer_fields), nrow(integer_fields)*.7))
+train <- integer_fields[dt,]
+test <- integer_fields[-dt,]
+
+#Modelling
+model <- svm(INCOME ~ ., data = train)
+
+#Predicted value
+train$predict <- model$fitted
+#INCOME mean
+INCOME_train_mean <- mean(train$INCOME)
+#Total Sum of Squares
+tss_train <- sum((train$INCOME - INCOME_train_mean)^2)
+#Residuals
+train$residuals <- train$INCOME - train$predict
+#Residual Sum of Squares
+rss_train <- sum(train$residuals^2)
+#R-squared
+rsq_train <- 1 - (rss_train/tss_train)
+#RMSE Test
+rmse_train <- sqrt(mean(train$residuals^2))
+
+#Plotting prediction vs INCOME
+ggplot(train, aes(x = predict, y = INCOME)) +   geom_point() +   geom_abline()
+
+#Fine tuning
+tuneResult <- tune(svm, INCOME ~ ., data = train, ranges = list(epsilon = seq(0,1,0.1), cost = 2^(2:9)))
+
+#Best Model
+best_model <- tuneResult$best.model
+#Predicted value
+train$predict_best <- predict(best_model, train)
+#INCOME mean
+INCOME_train_mean <- mean(train$INCOME)
+#Total Sum of Squares
+tss_train <- sum((train$INCOME - INCOME_train_mean)^2)
+#Residuals
+train$residuals <- train$INCOME - train$predict_best
+#Residual Sum of Squares
+rss_train <- sum(train$residuals^2)
+#R-squared
+rsq_train <- 1 - (rss_train/tss_train)
+#RMSE Test
+rmse_train <- sqrt(mean(train$residuals^2))
+#SVM Plot
+plot(tuneResult)
+
+
